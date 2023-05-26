a4cb9d739df1ead3d07e8865fc55550a9a941a12
Alexey Presniakov <me@klad.me>
2023-05-26 12:10:15 +0300

    Entrypoint fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 
 if [ `whoami` == "root" ]; then
-	groupmod -g $REAL_GROUP_ID user
-	usermod -u $REAL_USER_ID user
+	if [ "$REAL_GROUP_ID" != "" ]; then
+		groupmod -g $REAL_GROUP_ID user
+	fi
+	if [ "$REAL_USER_ID" != "" ]; then
+		usermod -u $REAL_USER_ID user
+	fi
 	sudo -u user docker-entrypoint.sh $@
 else
 	source ~/.profile
